ca33284be39e176c7fa3ebc81e8cfcb2ea825b91
LadyLashayDaProgrammer <168157196+LadyLashayDaProgrammer@users.noreply.github.com>
2024-04-28 22:07:33 -0500

    Old Portfolio
    Added website files application, assets, and system, which use codeigniter.

---
+++ application/views/pages/about.php
@@ -0,0 +1,38 @@
+<!-------------------------------------
+Name: Gennette Sanders
+Purpose: home page.
+---------------------------------------->
+			
+			<div class="container-fluid">
+				<h1 class="neonText2">About Me</h1><br><b>	
+				<div class="mx-auto">
+					<div class="card text-center" style="width:100%;, height:100%;" id="bkg-color">
+						<div class="text-center">
+							<img class="card-img-top rounded-circle" style="width:50%;, height:50%" src="assets/images/Graduation.jpg" alt="Graduation"/>
+						</div>
+						<div class="card-body">
+							<p class="card-text">A Computer Programmer and Web Designer from Austin, Texas 
+								that strives to bring you the best customized, and functional websites. I achieved 
+								an associate degree in Computer Programming from Austin Community College and fell in 
+								love with web development and design. Along the way, I noticed my family and friends were
+								becoming small business owners. That’s why I started Designs By Nette. Small business owners deserve 
+								attractive websites exactly like huge businesses. With that in mind, I do not believe in overcharging you for 
+								the work I provide. After discussing your needs, I will provide you with a customized quote depending on your 
+								business's strategy. I understand budget constraints and will work with your business to deliver a final project
+								that meets your budget. 	
+							</p>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item" id="bkg-color">I Create Attractive Designs Using Cascading Style Sheet(CSS) and Boostrap CSS Framework.</li>
+							<li class="list-group-item" id="bkg-color">I Write Amazing Markups Using HTML, Object-Oriented Language (JavaScript and Python), 
+								Scripting Language(PHP), Java Server Pages (JSP), SQL, and API (Application Programming Interface).</li>
+							<li class="list-group-item" id="bkg-color">I Make Websites Functional and Useful using Codeigniter Framework and Mustache Template.</li>
+						</ul>
+						<div class="card-body">
+							<div class="text-center">
+								<button type="button" class="neonButton"><a href="https://www.parchment.com/u/award/dad33dbcb9168a79a3754ff09df9711e" role="button" class="card-link">My Diploma</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
